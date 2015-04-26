abd6cd0cd8da526a1be613c3a4b047b799418b1e
Alexander Saar <asaar@adobe.com>
2015-04-26 21:02:17 +0200

    reformatting

--- Jslurp.rb
+++ Jslurp.rb
@@ -1,27 +1,22 @@
 require 'formula'
 
 # Homebrew formula to install JIRA slurper
-class JSlurp < Formula
+class Jslurp < Formula
 
-    version "1.0.0"
-    def release() "1" end   # custom release field
-
-    homepage 'https://github.com/alexsaar/jslurp'
-    url "https://raw.githubusercontent.com/alexsaar/jslurp/master/src/jslurp.groovy"
-    sha1 'fd332c93a5b63fd3f032f6d998d0c2cf4e64694d'
-
-    # dependencies
-    depends_on "groovy"
+  homepage 'https://github.com/alexsaar/jslurp'
+  url "https://raw.githubusercontent.com/alexsaar/jslurp/master/src/jslurp.groovy"
+  sha1 'fd332c93a5b63fd3f032f6d998d0c2cf4e64694d'
+  version "1.0.0"
+  
+  depends_on "groovy"
     
-    # Install
-    def install
-        # move to bin folder
-        puts "Moving stuff to bin..."
-        Dir.mkdir "bin"
+  def install
+    # move to bin folder
+    puts "Moving stuff to bin..."
+    Dir.mkdir "bin"
+    Dir['jslurp.groovy'].each { |f| mv f, "bin/#{f.gsub('.groovy', '')}" }
         
-        Dir['jslurp.groovy'].each { |f| mv f, "bin/#{f.gsub('.groovy', '')}" }
-        
-        prefix.install_metafiles
-        prefix.install Dir['*']
-    end
+    prefix.install_metafiles
+    prefix.install Dir['*']
+  end
 end
