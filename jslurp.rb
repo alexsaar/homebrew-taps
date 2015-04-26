e5890bee62a9ae4d26dc7ebe7e18310032283ccc
Alexander Saar <asaar@adobe.com>
2015-04-26 20:05:56 +0200

    update homepage

--- jslurp.rb
+++ jslurp.rb
@@ -6,7 +6,7 @@ class JSlurp < Formula
     version "1.0.0"
     def release() "1" end   # custom release field
 
-    homepage 'https://github.com/alexsaar/taps/wiki/JIRA-Slurper'
+    homepage 'https://github.com/alexsaar/jslurp'
     url "https://raw.githubusercontent.com/alexsaar/jslurp/master/src/jslurp.groovy"
     sha1 'fd332c93a5b63fd3f032f6d998d0c2cf4e64694d'
 
